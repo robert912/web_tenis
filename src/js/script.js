4f9b9a540bf5c4bcb1610a7b5f28f48dca515435
Roberto <roberto.orellana.t@usch.cl>
2025-03-09 23:34:21 -0300

    carrucel y cart shopping

--- src/js/script.js
+++ src/js/script.js
@@ -5,18 +5,34 @@ document.addEventListener('DOMContentLoaded', () => {
     let listCartHTML = document.querySelector('.listCart');
     let iconCart = document.querySelector('.icon-cart');
     let iconCartSpan = document.querySelector('.icon-cart span');
-    let body = document.querySelector('body');
     let closeCart = document.querySelector('.close');
     let cartTab = document.querySelector('.cartTab');
     let products = [];
     let cart = [];
     // const pageTitle = document.querySelector('.page-title');
+    const topBar = document.getElementById('top-bar');
+    const dropdownMenu = document.querySelector('.dropdown-menu');
+    const body = document.body;
+
+    // Detectar el evento de scroll y aplicar la clase 'scrolled'
+    const handleScroll = () => {
+        if (window.scrollY > 50) {
+            topBar.classList.add('scrolled');
+            dropdownMenu.classList.add('scrolled');
+        } else {
+            topBar.classList.remove('scrolled');
+            dropdownMenu.classList.remove('scrolled');
+        }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
     iconCart.addEventListener('click', () => {
         cartTab.classList.toggle('show');
-    })
+    });
     closeCart.addEventListener('click', () => {
         cartTab.classList.toggle('show');
-    })
+    });
 
     window.addToCart = (product_id) => {
         let positionThisProductInCart = cart.findIndex((value) => value.product_id == product_id);
@@ -33,12 +49,25 @@ document.addEventListener('DOMContentLoaded', () => {
         }else{
             cart[positionThisProductInCart].quantity = cart[positionThisProductInCart].quantity + 1;
         }
+
+        // Obtener el nombre del producto
+        let positionProduct = products.findIndex((value) => value.id == product_id);
+        let info = products[positionProduct];
+        let productName = info.name;
+
+        // Configurar Toastr para incluir el botón de cierre
+        toastr.options = {
+            "closeButton": true,
+        };
+        toastr.success(`¡Has añadido ${productName} a tu carrito de compra!`, "Felicidades!");
         addCartToHTML();
         addCartToMemory();
-    }
+    };
+
     window.addCartToMemory = () => {
         localStorage.setItem('cart', JSON.stringify(cart));
-    }
+    };
+
     window.addCartToHTML = () => {
         listCartHTML.innerHTML = '';
         let totalQuantity = 0;
@@ -69,11 +98,11 @@ document.addEventListener('DOMContentLoaded', () => {
                         <a href="#" class="delete-btn"><i class="fa-solid fa-trash-can"></i></a>
                     </div>
                 `;
-            })
+            });
         }
         iconCartSpan.innerText = totalQuantity;
-    }
-    
+    };
+
     listCartHTML.addEventListener('click', (event) => {
         let positionClick = event.target;
         if(positionClick.classList.contains('minus') || positionClick.classList.contains('plus')){
@@ -110,7 +139,7 @@ document.addEventListener('DOMContentLoaded', () => {
         }
         addCartToHTML();
         addCartToMemory();
-    }
+    };
 
     window.removeFromCart = (product_id) => {
         let positionItemInCart = cart.findIndex((value) => value.product_id == product_id);
@@ -119,8 +148,8 @@ document.addEventListener('DOMContentLoaded', () => {
         }
         addCartToHTML();
         addCartToMemory();
-    }
-    
+    };
+
     const initApp = () => {
         // get data product
         fetch('products.json')
@@ -132,14 +161,15 @@ document.addEventListener('DOMContentLoaded', () => {
                 cart = JSON.parse(localStorage.getItem('cart'));
                 addCartToHTML();
             }
-        })
-    }
+        });
+    };
     initApp();
 
     // Función para cargar el contenido de la página
     loadPage = async function(page) {
-        console.log(page)
-        if (page === null || page === 'salir'){return 0}
+        console.log(page);
+        if (page === null || page === 'salir'){return 0;}
+        page === 'inicio' ? body.classList.add('inicio') : body.classList.remove('inicio');
         try {
             // Simulamos la carga de contenido
             const response = await fetch(`src/pages/${page}.html`);
@@ -156,12 +186,12 @@ document.addEventListener('DOMContentLoaded', () => {
                 })
                 .catch((error) => {
                     console.error('Error al cargar el script de inicio:', error);
-                })
+                });
         } catch (e) {
             console.error('Error al cargar la página:', e);
             content.innerHTML = '<p>Error al cargar el contenido. Por favor, intenta de nuevo.</p>';
         }
-    }
+    };
 
     function removeScript(url) {
         const existingScript = document.querySelector(`script[src="${url}"]`);
@@ -171,7 +201,7 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     function loadScript(url) {
-        removeScript(url)
+        removeScript(url);
         return new Promise((resolve, reject) => {
             const script = document.createElement('script');
             script.src = url;
@@ -197,7 +227,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Cargar la página de inicio por defecto
     currentPage = sessionStorage.getItem('page');
-    loadPage(currentPage? currentPage : 'inicio');
+    loadPage(currentPage ? currentPage : 'inicio');
 });
 
 function formatearValorPesos(numero) {
